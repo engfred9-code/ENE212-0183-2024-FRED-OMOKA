8bf71373a00d99218fa329e25e180a806aaaa5ff
engfred9-code <engfred9@gmail.com>
2025-11-16 11:04:30 +0300

    Assignment 3

---
+++ Assignment 1/Assignment 2/Assignment 3/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    char name[50] ,reg[50] ,course[50];
+    printf("Enter your name:");
+    scanf(" %s",name);
+    printf("Your name is: %s\n",name);
+    printf("Enter your reg:");
+    scanf(" %s",reg);
+    printf("Your reg is: %s\n",reg);
+    printf("Enter your course:");
+    scanf(" %s",course);
+    printf("Your course is: %s\n",course);
+
+
+    return 0;
+
+}
