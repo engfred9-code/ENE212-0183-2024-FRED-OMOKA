b19c0bcd0b2617d7de1a07648a6474582066d199
engfred9-code <engfred9@gmail.com>
2025-11-16 10:55:39 +0300

    Create main.c

---
+++ Assignment 1/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main()
+{
+int A=0;
+int B=0;
+printf("A:");
+scanf("%d",&A);
+printf("B:");
+scanf("%d",&B);
+
+int addition=A+B;
+int subtraction=A-B;
+int multiplication=A*B;
+int division=A/B;
+int modulus=A%B;
+
+printf("value of addition:%d\n",addition);
+printf("value of subtraction:%d\n",subtraction);
+printf("value of multiplication:%d\n",multiplication);
+printf("value of division:%d\n",division);
+printf("value of modulus:%d\n",modulus);
+
+return 0;
+
+}
+
+
+
+
+
+
+
+
+
